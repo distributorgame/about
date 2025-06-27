329422eefdf9691aa7659369ebf029550d39cab5
distributorgame <raincitygamingid@gmail.com>
2025-06-27 22:51:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["ANDROID and iOS ", "root - non-root", "non-jailbreak"],
+    strings: ["ANDROID and iOS ", "root and non root", "non jailbreak"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
