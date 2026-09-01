22b318530a242d666a49782a322b4ef11780783c
distributorgame <raincitygamingid@gmail.com>
2026-09-02 01:00:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["ANDROID and iOS ", "root and non root", "non jailbreak"],
+    strings: ["mau tau cara ngelupain dia??? ", "lupakan saja seperti caramu melupakan tuhanmu ", "jangan lupa ceritakan pada circlemu kalo akulah perusak hati tulusmu itu ", "available jual beli musang hitam],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
