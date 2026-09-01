3ee2cd61ae1a695fd71810c9ecb7b3391bc8ba78
distributorgame <raincitygamingid@gmail.com>
2026-09-02 01:06:07 +0700

    Fix string formatting in Typed.js initialization

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["mau tau cara ngelupain dia??? ", "lupakan saja seperti caramu melupakan tuhanmu ", "jangan lupa ceritakan pada circlemu kalo akulah perusak hati tulusmu itu ", "available jual beli musang hitam],
+    strings: ["mau tau cara ngelupain dia??? ", "lupakan saja seperti caramu melupakan tuhanmu ", "jangan lupa ceritakan pada circlemu kalo akulah perusak hati tulusmu itu ", "available jual beli musang hitam "],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
